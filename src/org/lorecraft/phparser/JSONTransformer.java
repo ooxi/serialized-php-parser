f8974fe52efc22f9d442e5fa7c2845384321fa70
ooxi <violetland@mail.ru>
2015-03-03 11:04:08 +0100

    Format source code to comply with Sun Coding Standards

--- src/org/lorecraft/phparser/JSONTransformer.java
+++ src/org/lorecraft/phparser/JSONTransformer.java
@@ -7,49 +7,38 @@ import java.util.Map.Entry;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-public class JSONTransformer
-{
+public class JSONTransformer {
 
-  @SuppressWarnings("rawtypes")
-  public static Object toJSON(Object o)
-  {
-    if (o instanceof Map)
-    {
-      return arrayToJSON((Map) o);
-    }
-    else if (o instanceof SerializedPhpParser.PhpObject)
-    {
-      return mapToJSON(((SerializedPhpParser.PhpObject) o).attributes);
-    }
-    else if (o == SerializedPhpParser.NULL)
-    {
-      return null;
-    }
-    return o;
-  }
+	@SuppressWarnings("rawtypes")
+	public static Object toJSON(Object o) {
+		if (o instanceof Map) {
+			return arrayToJSON((Map) o);
+		} else if (o instanceof SerializedPhpParser.PhpObject) {
+			return mapToJSON(((SerializedPhpParser.PhpObject) o).attributes);
+		} else if (o == SerializedPhpParser.NULL) {
+			return null;
+		}
+		return o;
+	}
 
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  private static JSONArray arrayToJSON(Map o)
-  {
-    JSONArray a = new JSONArray();
-    for (Object obj : o.values())
-    {
-      a.add(toJSON(obj));
-    }
-    return a;
-  }
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	private static JSONArray arrayToJSON(Map o) {
+		JSONArray a = new JSONArray();
+		for (Object obj : o.values()) {
+			a.add(toJSON(obj));
+		}
+		return a;
+	}
 
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  private static JSONObject mapToJSON(Map o)
-  {
-    JSONObject obj = new JSONObject();
-    Map map = o;
-    Iterator<Map.Entry> i = map.entrySet().iterator();
-    while (i.hasNext())
-    {
-      Entry next = i.next();
-      obj.put(next.getKey(), toJSON(next.getValue()));
-    }
-    return obj;
-  }
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	private static JSONObject mapToJSON(Map o) {
+		JSONObject obj = new JSONObject();
+		Map map = o;
+		Iterator<Map.Entry> i = map.entrySet().iterator();
+		while (i.hasNext()) {
+			Entry next = i.next();
+			obj.put(next.getKey(), toJSON(next.getValue()));
+		}
+		return obj;
+	}
 }
